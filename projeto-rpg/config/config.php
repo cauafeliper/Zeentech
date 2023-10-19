f29c31ead9928b8dbc5f0f78c9e9b7dee1a666b8
kuazin <cauafelipe2003@hotmail.com>
2023-10-19 14:22:40 -0300

    começo do projeto-rpg

---
+++ projeto-rpg/config/config.php
@@ -0,0 +1,8 @@
+<?php 
+    $dbHost = 'Localhost';
+    $dbUsername = 'root';
+    $dbPassword = '';
+    $dbName = 'rpg';
+
+    $conexao = new mysqli($dbHost, $dbUsername, $dbPassword, $dbName);
+?>
