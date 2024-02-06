9ea7814c196ae362c941430a9ead3377cb7059bd
Lucas Moreira Leckar <lucasmleckar@hotmail.com>
2024-02-06 11:22:22 -0300

    botao verificar

--- Agendamento-RPG/cadastro/verificar_login.php
+++ Agendamento-RPG/cadastro/verificar_login.php
@@ -1,3 +1,10 @@
+<?php
+    include_once('../config/config.php');
+    session_start();
+
+    date_default_timezone_set('America/Sao_Paulo'); // Define o fuso horário para São Paulo 
+?>
+
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
@@ -11,88 +18,91 @@
 </head>
 <body>
     <main>
-
-<?php
-    include_once('../config/config.php');
-    session_start();
-
-    date_default_timezone_set('America/Sao_Paulo'); // Define o fuso horário para São Paulo
-
-    if(isset($_GET['token'])) {
-        $token = $_GET['token'];
-
-        // Verificar se o token existe na tabela logins_pendentes
-        $stmt = $conexao->prepare("SELECT * FROM logins_pendentes WHERE token = ? AND expiracao > NOW()");
-        $stmt->bind_param("s", $token);
-        $stmt->execute();
-        $result = $stmt->get_result();
-        $stmt->close();
-
-        if($result->num_rows > 0) {
-            $row = $result->fetch_assoc();
-
-            // Inserir na tabela logins
-            $stmt_insert = $conexao->prepare("INSERT INTO logins (numero, nome, empresa, area, email, senha) VALUES (?, ?, ?, ?, ?, ?)");
-            $stmt_insert->bind_param("ssssss", $row['numero'], $row['nome'], $row['empresa'], $row['area'], $row['email'], $row['senha']);
-            $stmt_insert->execute();
-            $stmt_insert->close();
-
-            // Excluir da tabela logins_pendentes
-            $stmt_delete = $conexao->prepare("DELETE FROM logins_pendentes WHERE token = ?");
-            $stmt_delete->bind_param("s", $token);
-            $stmt_delete->execute();
-            $stmt_delete->close();
-
-            echo '<script>
-                    Swal.fire({
-                        icon: "success",
-                        title: "Email Verificado!",
-                        text: "Seu email foi verificado com sucesso. Agora você pode fazer login.",
-                        confirmButtonText: "Login",
-                        confirmButtonColor: "#001e50",
-                        allowOutsideClick: false,
-                    }).then((result) => {
-                        if (result.isConfirmed) {
-                            // Redireciona o usuário para a página de login
-                            window.location.href = "../index.php";
-                        }
-                    });
-                </script>';
-        } else {
-            echo '<script>
-                    Swal.fire({
-                        icon: "warning",
-                        title: "Token Inválido!",
-                        text: "O token fornecido não é válido ou expirou.",
-                        confirmButtonText: "OK",
-                        confirmButtonColor: "#001e50",
-                        allowOutsideClick: false,
-                    }).then((result) => {
-                        if (result.isConfirmed) {
-                            // Redireciona o usuário para a página de login
-                            window.location.href = "../index.php";
-                        }
-                    });
-                </script>';
-        }
-    } else {
-        echo '<script>
-                Swal.fire({
-                    icon: "warning",
-                    title: "Token Ausente!",
-                    text: "O token não foi fornecido.",
-                    confirmButtonText: "OK",
-                    confirmButtonColor: "#001e50",
-                    allowOutsideClick: false,
-                }).then((result) => {
-                    if (result.isConfirmed) {
-                        // Redireciona o usuário para a página de login
-                        window.location.href = "../index.php";
+        <?php
+            if(isset($_GET['token'])) {
+                $token = $_GET['token'];
+                if ($_SERVER['REQUEST_METHOD'] === 'GET' && isset($_GET['submit'])){       
+                    // Verificar se o token existe na tabela logins_pendentes
+                    $stmt = $conexao->prepare("SELECT * FROM logins_pendentes WHERE token = ? AND expiracao > NOW()");
+                    $stmt->bind_param("s", $token);
+                    $stmt->execute();
+                    $result = $stmt->get_result();
+                    $stmt->close();
+            
+                    if($result->num_rows > 0) {
+                        $row = $result->fetch_assoc();
+            
+                        // Inserir na tabela logins
+                        $stmt_insert = $conexao->prepare("INSERT INTO logins (numero, nome, empresa, area, email, senha) VALUES (?, ?, ?, ?, ?, ?)");
+                        $stmt_insert->bind_param("ssssss", $row['numero'], $row['nome'], $row['empresa'], $row['area'], $row['email'], $row['senha']);
+                        $stmt_insert->execute();
+                        $stmt_insert->close();
+            
+                        // Excluir da tabela logins_pendentes
+                        $stmt_delete = $conexao->prepare("DELETE FROM logins_pendentes WHERE token = ?");
+                        $stmt_delete->bind_param("s", $token);
+                        $stmt_delete->execute();
+                        $stmt_delete->close();
+            
+                        echo '<script>
+                                Swal.fire({
+                                    icon: "success",
+                                    title: "Email Verificado!",
+                                    text: "Seu email foi verificado com sucesso. Agora você pode fazer login.",
+                                    confirmButtonText: "Login",
+                                    confirmButtonColor: "#001e50",
+                                    allowOutsideClick: false,
+                                }).then((result) => {
+                                    if (result.isConfirmed) {
+                                        // Redireciona o usuário para a página de login
+                                        window.location.href = "../index.php";
+                                    }
+                                });
+                            </script>';
+                    } else {
+                        echo '<script>
+                                Swal.fire({
+                                    icon: "warning",
+                                    title: "Token Inválido!",
+                                    text: "O token fornecido não é válido ou expirou.",
+                                    confirmButtonText: "OK",
+                                    confirmButtonColor: "#001e50",
+                                    allowOutsideClick: false,
+                                }).then((result) => {
+                                    if (result.isConfirmed) {
+                                        // Redireciona o usuário para a página de login
+                                        window.location.href = "../index.php";
+                                    }
+                                });
+                            </script>';
                     }
-                });
-            </script>';
-    }
-?>
+                    exit();
+                }
+            } else {
+                echo '<script>
+                        Swal.fire({
+                            icon: "warning",
+                            title: "Token Ausente!",
+                            text: "O token não foi fornecido.",
+                            confirmButtonText: "OK",
+                            confirmButtonColor: "#001e50",
+                            allowOutsideClick: false,
+                        }).then((result) => {
+                            if (result.isConfirmed) {
+                                // Redireciona o usuário para a página de login
+                                window.location.href = "../index.php";
+                            }
+                        });
+                    </script>';
+            }
+        ?>
+
+        <form method="GET" action="<?php echo $_SERVER['PHP_SELF']; ?>">
+            <input type="hidden" name="token" value="<?php echo isset($_GET['token']) ? htmlspecialchars($_GET['token']) : ''; ?>">
+            <input type="submit" name="submit" value="verificar">
+        </form>
+
+
 
     </main>
 </body>
