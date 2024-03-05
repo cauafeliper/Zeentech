afa48f9e7181c7954a67c4efc557d382fd2e73d1
Lucas Moreira Leckar <lucasmleckar@hotmail.com>
2024-03-05 14:29:06 -0300

    config

--- Agendamento-RPG/config/config.php
+++ Agendamento-RPG/config/config.php
@@ -1,7 +1,7 @@
 <?php 
-    $dbHost = 'Localhost';
-    $dbUsername = 'root';
-    $dbPassword = '';
-    $dbName = 'rpg';
+    $dbHost = 'localhost';
+    $dbUsername = 'equipz30_idt2023';
+    $dbPassword = 'C@chorro69';
+    $dbName = 'equipz30_rpg';
 
     $conexao = new mysqli($dbHost, $dbUsername, $dbPassword, $dbName);
